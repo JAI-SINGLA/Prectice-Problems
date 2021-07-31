65b8e3039014bf91b77310541016fded9e600e7f
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-07-31 15:33:49 +0530

    Update FindMissingAndRepeating.cpp

--- Arrays/FindMissingAndRepeating.cpp
+++ Arrays/FindMissingAndRepeating.cpp
@@ -1,24 +1,53 @@
-class Solution {
+class Solution{
 public:
-    void sortColors(vector<int>& nums) {
-        int low=0;
-        int mid=0;
-        int n=nums.size();
-        int high=n-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[low],nums[mid]);
-                mid++;
-                low++;
-            }
-            else if(nums[mid]==1){
-                mid++;
-            }
-            else{
-                swap(nums[mid],nums[high]);
-                high--;
-            }
+    int *findTwoElement(int *arr, long long n) {
+        // code here
+        long long arrSum=0;
+        long long duplicate;
+        long long missing;
+        for(long long i=0;i<n;i++){
+            arrSum+=(long long)arr[i];
         }
+        long long simpleSum=n*(n+1)/2;
+        long long arrSquSum=0;
+        long long a=(arrSum-simpleSum);
+        for(long long i=0;i<n;i++){
+            arrSquSum+=((long long)arr[i]*(long long)arr[i]);
+        }
+        long long squSum=n*(n+1)*(2*(n)+1)/6;
+        long long b=(arrSquSum-squSum)/a;
+        duplicate=(a+b)/2;
+        missing=(b-a)/2;
+        int *ans=new int[2];
+        ans[0]=duplicate;
+        ans[1]=missing;
+        return ans;
         
     }
 };
+
+
+/* Another Solution
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        // code here
+        int * a= new int[2];
+        for(int i=0;i<n;i++){
+            if(arr[abs(arr[i])-1]>0){
+                arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
+            }
+            else{
+                a[0]=abs(arr[i]);
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(arr[i-1]>0){
+                a[1]=i;
+                break;
+            }
+        }
+        return a;
+    }
+};
+*/
