1aeeee46b2d2061e895473e5ab79cd440ac20a3a
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-01 12:41:21 +0530

    Update SetMatrixZeros.cpp

--- Arrays/SetMatrixZeros.cpp
+++ Arrays/SetMatrixZeros.cpp
@@ -29,3 +29,42 @@ public:
         }
     }
 };
+
+/* O(1) Approach
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        bool column=true;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                column=false;
+            }
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=row-1;i>=0;i--){
+            for(int j=col-1;j>=0;j--){
+                if(j==0){
+                    if(!column){
+                        matrix[i][j]=0;
+                    }
+                }
+                else{
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        
+    }
+};
+*/
