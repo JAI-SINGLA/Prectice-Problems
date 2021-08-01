ffd42f058b1e09458f5c81b6dbcb35cb285c7ef7
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-01 17:50:48 +0530

    Create MAjorityElement[N3].cpp
    https://leetcode.com/problems/majority-element-ii/

---
+++ Array-Maths/MAjorityElement[N3].cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if(it->second>nums.size()/3){
+                ans.push_back(it->first);
+            }
+        }
+        return ans;
+    }
+};
