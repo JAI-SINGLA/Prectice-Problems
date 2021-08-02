68d9ddb5e003e69acf6de118d45b76ebc07d9903
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-02 11:35:50 +0530

    Update Pow(x,n).cpp

--- Array-Maths/Pow(x,n).cpp
+++ Array-Maths/Pow(x,n).cpp
@@ -16,3 +16,30 @@ public:
             return x*myPow(x*x,(n-1)/2);
     }
 };
+
+/* Iterative Solution
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0000;
+        long long z=abs(n);
+        while(z){
+            if(z%2==0){
+                x=x*x;
+                z=z/2;
+            }
+            else{
+                ans=ans*x;
+                z=z-1;
+            }
+        }
+        if(n<0){
+            return 1.000/ans;
+        }
+        return ans;
+        
+        
+        
+    }
+};
+*/
