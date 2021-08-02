e31d9be6895c391f00f68a39d700877fde62c079
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-02 11:59:19 +0530

    Update MAjorityElement[N3].cpp
    https://leetcode.com/problems/majority-element-ii/

--- Array-Maths/MAjorityElement[N3].cpp
+++ Array-Maths/MAjorityElement[N3].cpp
@@ -14,3 +14,46 @@ public:
         return ans;
     }
 };
+
+/* O(1) Space Solution
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int num1=-1;
+        int num2=-1;
+        int count1=0;
+        vector<int>ans;
+        int count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==num1) count1++;
+            else if(nums[i]==num2)count2++;
+            else if(count1==0){
+                num1=nums[i];
+                count1++;
+            }
+            else if(count2==0){
+                num2=nums[i];
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==num1)count1++;
+            else if(nums[i]==num2)count2++;
+        }
+        if(count1>nums.size()/3){
+            ans.push_back(num1);
+        }
+        if(count2>nums.size()/3){
+            ans.push_back(num2);
+        }
+        return ans;
+        
+    }
+};
+/*
