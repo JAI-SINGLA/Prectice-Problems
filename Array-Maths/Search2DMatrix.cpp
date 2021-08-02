4cab395fc4cfdc897021685ecf1cdff1cb03acc5
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-02 11:19:37 +0530

    Update Search2DMatrix.cpp
    https://leetcode.com/problems/search-a-2d-matrix/

--- Array-Maths/Search2DMatrix.cpp
+++ Array-Maths/Search2DMatrix.cpp
@@ -1,3 +1,4 @@
+//O(m+n);
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -13,3 +14,32 @@ public:
         return false;
     }
 };
+
+/* Binary Search Solution If Complete Marix is Sorted O(log(m*n))
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int column=matrix[0].size();
+        int high=(row*column-1);
+        int low=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(matrix[mid/column][mid%column]==target){
+                return true;
+            }
+            if(matrix[mid/column][mid%column]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            
+        }
+        return false;
+        
+        
+    }
+};
+
+/*
