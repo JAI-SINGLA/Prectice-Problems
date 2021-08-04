2b0a18b9743cd7e513e8e5f882e709aa6d210bac
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-05 00:12:32 +0530

    Update LongestConsecutiveSequence.cpp
    https://leetcode.com/problems/longest-consecutive-sequence

--- Hashing/LongestConsecutiveSequence.cpp
+++ Hashing/LongestConsecutiveSequence.cpp
@@ -27,3 +27,33 @@ public:
         
     }
 };
+
+/* O(n) Solution and no changes in original Array
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]=1;
+        }
+        int i=0;
+        int currentStreak=0;
+        int maxStreak=0;
+        while(i<nums.size()){
+            if(mp.count(nums[i]-1)<=0){
+                currentStreak=1;
+                int temp=nums[i]+1;
+                while(mp.count(temp)>0){
+                    currentStreak++;
+                    temp++;
+                }
+                maxStreak=max(maxStreak,currentStreak);
+            }
+            i++;
+        }
+        return maxStreak;
+        
+    }
+};
+*/
