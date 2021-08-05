24e298af465fa2b3eeb37faec5175557fb448284
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-05 13:20:07 +0530

    Create DeleteGivenNodeWhenNodeisGiven.cpp
    https://leetcode.com/problems/delete-node-in-a-linked-list/

---
+++ LinkedList/DeleteGivenNodeWhenNodeisGiven.cpp
@@ -0,0 +1,11 @@
+//O(1) Soluion
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode*temp=node->next;
+        node->val=node->next->val;
+        node->next=node->next->next;
+        delete temp;
+        
+    }
+};
