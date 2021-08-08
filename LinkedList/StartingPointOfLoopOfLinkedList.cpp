689da2642e3f034bd2efca90e7c4f5064b08859f
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-08 20:03:33 +0530

    Update StartingPointOfLoopOfLinkedList.cpp
    https://leetcode.com/problems/linked-list-cycle-ii/

--- LinkedList/StartingPointOfLoopOfLinkedList.cpp
+++ LinkedList/StartingPointOfLoopOfLinkedList.cpp
@@ -15,3 +15,29 @@ public:
         
     }
 };
+
+
+//O(1) Space solution
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode*slow=head;
+        ListNode*fast=head;
+        if(head==NULL || head->next==NULL){
+                return NULL;
+        }
+        do{
+            slow=slow->next;
+            fast=fast->next->next;
+        }while(fast!=slow && fast!=NULL && fast->next!=NULL );
+        if(fast==NULL || fast->next==NULL){
+            return NULL;
+        }
+        ListNode*temp=head;
+        while(temp!=fast){
+            temp=temp->next;
+            fast=fast->next;
+        }
+        return fast;
+    }
+};
