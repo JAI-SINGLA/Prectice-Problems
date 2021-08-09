b86d2ea5048ead9bc385debf7cbd2d840263f5cd
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-09 15:30:02 +0530

    Create TrappingRainWater.cpp
    https://leetcode.com/problems/trapping-rain-water/

---
+++ 2-Pointer/TrappingRainWater.cpp
@@ -0,0 +1,23 @@
+//O(n) Space solution
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int>leftMax(n);
+        vector<int>rightMax(n);
+        leftMax[0]=height[0];
+        for(int i=1;i<height.size();i++){
+            leftMax[i]=max(leftMax[i-1],height[i]);
+        }
+        rightMax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightMax[i]=max(rightMax[i+1],height[i]);
+        }
+        int ans=0;
+        for(int i=1;i<height.size()-1;i++){
+            ans+=(min(rightMax[i],leftMax[i])-height[i]);
+        }
+        return ans;
+        
+    }
+};
