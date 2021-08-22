26e39d3b36d8a22af664650fec4c3536ef3a68da
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-22 17:17:35 +0530

    Update ImplementStackUsingQueue.cpp
    https://leetcode.com/problems/implement-stack-using-queues/

--- Stack and Queues/ImplementStackUsingQueue.cpp
+++ Stack and Queues/ImplementStackUsingQueue.cpp
@@ -47,3 +47,48 @@ public:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+
+//Using a Single Queue
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    queue<int>q1;
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+     q1.push(x);
+        for(int i=1;i<q1.size();i++){
+            q1.push(q1.front());
+            q1.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int x=q1.front();
+        q1.pop();
+        return x;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q1.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q1.size()==0?true:false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
