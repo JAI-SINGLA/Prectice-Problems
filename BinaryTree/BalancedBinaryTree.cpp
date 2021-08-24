8d04daee164a11fed01f40ec1c1feae5970a4f1f
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-24 11:44:56 +0530

    Update BalancedBinaryTree.cpp

--- BinaryTree/BalancedBinaryTree.cpp
+++ BinaryTree/BalancedBinaryTree.cpp
@@ -27,3 +27,43 @@ public:
         return isBalanced(root->left) && isBalanced(root->right);
     }
 };
+
+
+// O(n) Solution
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<bool,int>isBalancedHelper(TreeNode*root){
+        if(root==NULL){
+            pair<bool,int>p({true,0});
+            return p;
+        }
+        pair<bool,int>p1;
+        pair<bool,int>p2;
+        p1=isBalancedHelper(root->left);
+        p2=isBalancedHelper(root->right);
+        bool check=true;
+        if(abs(p1.second-p2.second)>1) check=false;
+        int height=1+max(p1.second,p2.second);
+        check= check&&p1.first&&p2.first;
+        pair<bool,int>finalP({check,height});
+        return finalP;
+        
+        
+    }
+    bool isBalanced(TreeNode* root) {
+        return isBalancedHelper(root).first;
+    }
+};
