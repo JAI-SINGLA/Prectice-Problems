05663641724df939de27d9537cadd0d141fd3885
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-24 11:49:07 +0530

    Update BalancedBinaryTree.cpp
    https://leetcode.com/problems/balanced-binary-tree/

--- BinaryTree/BalancedBinaryTree.cpp
+++ BinaryTree/BalancedBinaryTree.cpp
@@ -67,3 +67,32 @@ public:
         return isBalancedHelper(root).first;
     }
 };
+
+// Another O(n) Solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int heightModified(TreeNode*root){
+        if(root==NULL) return 0;
+        int lh=heightModified(root->left);
+        if(lh==-1) return -1;
+        int rh=heightModified(root->right);
+        if(rh==-1)return -1;
+        if(abs(rh-lh)>1)return -1;
+        return 1+max(lh,rh);
+    }
+    bool isBalanced(TreeNode* root) {
+        return heightModified(root)==-1?false:true;
+        
+    }
+};
