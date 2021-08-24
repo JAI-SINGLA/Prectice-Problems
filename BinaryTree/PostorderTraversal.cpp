101ada780c3797f7f38deda5d4a8eecbbe92ab6e
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-24 09:32:14 +0530

    Update PostorderTraversal.cpp
    https://leetcode.com/problems/binary-tree-postorder-traversal

--- BinaryTree/PostorderTraversal.cpp
+++ BinaryTree/PostorderTraversal.cpp
@@ -59,3 +59,50 @@ public:
         return ans;
     }
 };
+
+// Iterative single stack solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root==NULL)return ans;
+        stack<TreeNode*>st;
+        TreeNode*curr=root;
+        while(curr!=NULL || !st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                TreeNode*temp=st.top()->right;
+                if(temp==NULL){
+                    ans.push_back(st.top()->val);
+                    temp=st.top();
+                    st.pop();
+                    while(!st.empty()&&st.top()->right==temp){
+                        ans.push_back(st.top()->val);
+                        temp=st.top();
+                        st.pop();
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
