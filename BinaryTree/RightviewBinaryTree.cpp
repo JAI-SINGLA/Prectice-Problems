bb7eba2a24b69adabbaa67975860acd87ee73b5b
Jai Singla <62647200+JAI-SINGLA@users.noreply.github.com>
2021-08-24 12:35:57 +0530

    Update RightviewBinaryTree.cpp
    https://leetcode.com/problems/binary-tree-right-side-view/

--- BinaryTree/RightviewBinaryTree.cpp
+++ BinaryTree/RightviewBinaryTree.cpp
@@ -30,3 +30,34 @@ public:
         
     }
 };
+
+// Recursive Solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void rightSideView(TreeNode*root,int level , int& maxLevel, vector<int>&ans){
+        if(root==NULL)return;
+        if(level>maxLevel){
+            ans.push_back(root->val);
+            maxLevel=level;
+        }
+        rightSideView(root->right,level+1,maxLevel,ans);
+        rightSideView(root->left,level+1,maxLevel,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        int maxLevel=0;
+        rightSideView(root,1,maxLevel,ans);
+        return ans;
+    }
+};
